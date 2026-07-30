40bee71633875a22b3afd7080300c9dc7203eeac
endotrizine <53936918+endotrizine@users.noreply.github.com>
2026-07-31 03:07:30 +0500

    remove xorg prefix

--- pkgs/koala-clash/default.nix
+++ pkgs/koala-clash/default.nix
@@ -74,20 +74,20 @@ stdenv.mkDerivation rec {
     libsecret
     systemd
 
-    xorg.libX11
-    xorg.libxcb
-    xorg.libXi
-    xorg.libXcursor
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXtst
-    xorg.libXScrnSaver
-    xorg.libXinerama
-    xorg.libXcomposite
-    xorg.libxshmfence
+    libX11
+    libxcb
+    libXi
+    libXcursor
+    libXdamage
+    libXext
+    libXfixes
+    libXrandr
+    libXrender
+    libXtst
+    libXScrnSaver
+    libXinerama
+    libXcomposite
+    libxshmfence
   ];
 
   unpackPhase = ''
